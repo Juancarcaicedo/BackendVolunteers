040f6950121151868381779544e974536d27373e
Juancarcaicedo <74690988+Juancarcaicedo@users.noreply.github.com>
2025-02-05 22:55:26 -0500

    Initial commit

---
+++ src/main/java/com/InternationalAssociationVolunteers/BackendVolunteers/BackendVolunteersApplication.java
@@ -0,0 +1,13 @@
+package com.InternationalAssociationVolunteers.BackendVolunteers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVolunteersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVolunteersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InternationalAssociationVolunteers/BackendVolunteers/BackendVolunteersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InternationalAssociationVolunteers.BackendVolunteers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVolunteersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
